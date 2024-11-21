a90f7702aa0a8d702e75a9ed033ec4a1c30bef07
Jeison SR <jeison.sr.cursos@gmail.com>
2024-11-21 20:01:52 -0300

    Finalização da configuração do MariaDB.

--- lista_de_tarefas_be_n/settings.py
+++ lista_de_tarefas_be_n/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'lista_de_tarefas_be_n.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': 'js1902r',
+        'NAME': 'lista_de_tarefas',
     }
 }
 
